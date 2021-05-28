20ca16f84bb5424ed4e42fabbcbeac67158e5637
Helena <helenasleterkesto@gmail.com>
2021-05-28 11:47:05 +0200

    Add hard data

---
+++ src/Components/ColourItems.js
@@ -0,0 +1,44 @@
+export const ColourItems = [
+    {
+        id: 1,
+        name: "currant red",
+        product: "DA0808",
+        price: 24.85,
+        volume: 50,
+    },
+    {
+        id: 2,
+        name: "forrest",
+        product: "1D8133",
+        price: 24.85,
+        volume: 50,
+    },
+    {
+        id: 3,
+        name: "yellow bronze",
+        product: "C69536",
+        price: 33.65,
+        volume: 50,
+    },
+    {
+        id: 4,
+        name: "ocean blue",
+        product: "0869DA",
+        price: 33.65,
+        volume: 50,
+    },
+    {
+        id: 5,
+        name: "bougainvillea",
+        product: "802DC1",
+        price: 21.95,
+        volume: 50,
+    },
+    {
+        id: 6,
+        name: "bright topaz",
+        product: "F3EE67",
+        price: 24.85,
+        volume: 50,
+    },
+];
