67eeeed1e2b7fa266eef37aa21f387519822ac51
Helena <helenasleterkesto@gmail.com>
2021-05-28 12:05:59 +0200

    Add routing. Tested routes with no errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Router, Route } from "react-router-dom";
+import Cart from "./Pages/Cart";
+import Colouritem from "./Pages/Colouritem";
+import Colours from "./Pages/Colours";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route path={"/"} exact>
+                    Home
+                </Route>
+                <Route path={"/colours"} exact>
+                    Colours
+                </Route>
+                <Route path={"/contact"} exact>
+                    Contact
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
