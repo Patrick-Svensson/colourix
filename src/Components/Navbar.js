68d990f9d8ece0a6c994cb595073582b0bdf487d
Patrick Svensson <ps.digitalepost@gmail.com>
2021-05-30 14:39:52 +0200

    Make navbar collapsable.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { BiCart } from "react-icons/bi";
 import { ReactComponent as ReactLogo } from "./ColourixLogo.svg";
 
 const Navbar = () => {
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+
     return (
-        <div className="navBar">
+        <div className="navbar">
             <div className="logoContainer">
                 <NavLink className="logoLink" exact to="/">
                     <ReactLogo className="logo" />
                 </NavLink>
             </div>
-            <div className="navLinkContainer">
+            <div className={sidebar ? "navLinkContainer active" : "navLinkContainer"}>
                 <NavLink className="navLink homeLink" activeClassName="activeHomeLink" exact to="/">
                     Home
                 </NavLink>
@@ -20,9 +25,16 @@ const Navbar = () => {
                 <NavLink className="navLink contactLink" activeClassName="activeContactLink" exact to="/contact">
                     Contact
                 </NavLink>
-                <NavLink className="navLink cartLink" activeClassName="activeCartLink" exact to="/cart">
-                    <BiCart />
-                </NavLink>
+            </div>
+            <NavLink className="navLink cartLink" activeClassName="activeCartLink" exact to="/cart">
+                <BiCart className="cartIcon" />
+            </NavLink>
+            <div className="menuToggleContainer">
+                <div className={sidebar ? "menu__toggle active" : "menu__toggle"} onClick={showSidebar}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
             </div>
         </div>
     );
