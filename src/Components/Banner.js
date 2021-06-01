4d9c6b67006d0d4e41af0cc38a18569bd28085ca
Patrick Svensson <ps.digitalepost@gmail.com>
2021-06-01 15:12:55 +0200

    Add style to Banner. Adjust letter spacing to Home headers.

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -6,7 +6,15 @@ const Banner = () => {
         <>
             <Link className="bannerLink" to="/colours">
                 <div className="banner">
-                    <div className=""></div>
+                    <div className="bannerContent">
+                        <h3 className="bannerHeading">
+                            24h sale &#45; <span>only today</span>
+                        </h3>
+                        <p className="bannerText">
+                            Get the dashing Bougainvillea for only<span className="bannerNewPrice"> &nbsp;€ 8.85&nbsp; </span>
+                            <span className="bannerPrice">€ 21.95</span>
+                        </p>
+                    </div>
                 </div>
             </Link>
         </>
