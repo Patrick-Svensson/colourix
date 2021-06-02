302c4688ac5eee97b58dd4b5ab8163823a98ca5c
Patrick Svensson <ps.digitalepost@gmail.com>
2021-06-02 23:21:48 +0200

    Fix merge conflict.

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -5,10 +5,7 @@ import "./Cart.css";
 
 const Cart = (props) => {
     const { cartItems, onAdd, onRemove } = props;
-    const itemsPrice = cartItems.reduce(
-        (accumulator, current) => accumulator + current.price * current.qty,
-        0
-    );
+    const itemsPrice = cartItems.reduce((accumulator, current) => accumulator + current.price * current.qty, 0);
 
     const totalPrice = itemsPrice;
 
@@ -33,42 +30,21 @@ const Cart = (props) => {
                         <Title title="my cart" />
                     </div>
                     <div className="cartContentContainer">
-                        <div>
-                            {cartItems.length === 0 && (
-                                <div className="cartCheckboxLabel text-center">
-                                    Cart is Empty
-                                </div>
-                            )}
-                        </div>
+                        <div>{cartItems.length === 0 && <div className="cartCheckboxLabel text-center">Cart is Empty</div>}</div>
                         {cartItems.map((item) => (
                             <div key={item.id} className="row">
-                                <div
-                                    className="productImg"
-                                    style={{ background: "#" + item.colour }}
-                                ></div>
-                                <div className="cartCheckboxLabel">
-                                    {item.name}
-                                </div>
+                                <div className="productImg" style={{ background: "#" + item.colour }}></div>
+                                <div className="cartCheckboxLabel">{item.name}</div>
                                 <div>
-                                    <button
-                                        onClick={() => onAdd(item)}
-                                        className="add"
-                                    >
+                                    <button onClick={() => onAdd(item)} className="add">
                                         +
                                     </button>
-                                    <button
-                                        onClick={() => onRemove(item)}
-                                        className="remove"
-                                    >
+                                    <button onClick={() => onRemove(item)} className="remove">
                                         -
                                     </button>
                                 </div>
-                                <div className="cartCheckboxLabel">
-                                    {item.qty}
-                                </div>
-                                <div className="cartCheckboxLabel flex-end">
-                                    € {item.price.toFixed(2)}
-                                </div>
+                                <div className="cartCheckboxLabel">{item.qty}</div>
+                                <div className="cartCheckboxLabel flex-end">€ {item.price.toFixed(2)}</div>
                             </div>
                         ))}
                         {cartItems.length !== 0 && (
@@ -79,37 +55,20 @@ const Cart = (props) => {
                                         <strong>Total</strong>
                                     </div>
                                     <div className="cartCheckboxLabel flex-end span-2">
-                                        <strong>
-                                            € {totalPrice.toFixed(2)}
-                                        </strong>
+                                        <strong>€ {totalPrice.toFixed(2)}</strong>
                                     </div>
                                 </div>
                             </>
                         )}
                     </div>
                     <div className="cartActionBar">
-<<<<<<< HEAD
-                        <Link to="./colours">
-                            <Button style={contShopBtnStyle} text="continue shopping" />
-                        </Link>
-=======
-                        <Button
-                            style={contShopBtnStyle}
-                            text="continue shopping"
-                        />
->>>>>>> 8e64ac7ce9745de4c7e80977f7c6897beaa0791e
+                        <Button style={contShopBtnStyle} text="continue shopping" />
                         <Button style={readTermsBtnStyle} text="read terms" />
                         <div className="cartCheckboxContainer">
-                            <label className="cartCheckboxLabel">
-                                accept terms
-                            </label>
+                            <label className="cartCheckboxLabel">accept terms</label>
                             <input type="checkbox" id="cartCheckbox" />
                         </div>
-                        <Button
-                            style={toCheckoutBtnStyle}
-                            text="to checkout"
-                            onClick={() => alert("Implement Checkout")}
-                        />
+                        <Button style={toCheckoutBtnStyle} text="to checkout" onClick={() => alert("Implement Checkout")} />
                     </div>
                 </div>
             </div>
