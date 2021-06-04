bc704c2e3f48f36f12be2170c6b79539f0507a3e
Helena <helenasleterkesto@gmail.com>
2021-06-04 08:42:03 +0200

    Positioning form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,83 +1,72 @@
-import Title from "../Components/Title"
-import Button from "../Components/Button"
-import "./Contact.css"
-import { findByLabelText } from "@testing-library/dom"
-
-
+import Title from "../Components/Title";
+import Button from "../Components/Button";
+import "./Contact.css";
+//import { findByLabelText } from "@testing-library/dom"
 
 const clearButton = {
-    backgroundColor: '#EB8D8D',
-}
-
-const sendButton = {
-    backgroundColor: '#9AEB8D',
-}
-
-
-const Contact = () => {
-    return <div className="contact">
-
-        <div className="wrapper">
-            <div className="adress">
-                <Title title="Contact" />
-            </div>
-
-            <div className="contact-form">
-                <form action="" class="form">
-                    <label className="cartContact" for="">First Name</label>
-                    <input type="text" />
-                </form>
-
-                <form action="" class="form">
-                    <label className="cartContact" for="">Last Name</label>
-                    <input type="text" />
-                </form>
-
-                <form action="" class="form">
-                    <label className="cartContact" for="">Email </label>
-                    <input type="text" />
-                </form>
-
-                <form action="" class="form">
-                    <label className="cartContact" for="">Masseg</label>
-                    <textarea type="text" />
-                </form>
-            </div>
-        </div>
-        <div className="btn-knapp">
-            <div className="btn-clear">
-                <Button className="cartContact" text="clear" style={clearButton} />
-            </div>
-            <div className="btn-esnd">
-                <Button className="cartContact" text="Send" style={sendButton} />
-            </div>
-        </div>
-    </div>
-
-
+    backgroundColor: "#EB8D8D",
 };
 
+const sendButton = {
+    backgroundColor: "#9AEB8D",
+};
 
+const Contact = () => {
+    return (
+        <div className="contact">
+            <div className="wrapper">
+                <div className="adress">
+                    <Title title="Contact" />
+                </div>
 
+                <div className="contact-form">
+                    <form action="" class="form">
+                        <label className="cartContact label" for="">
+                            First Name
+                        </label>
+                        <input type="text" />
+                    </form>
+
+                    <form action="" class="form">
+                        <label className="cartContact label" for="">
+                            Last Name
+                        </label>
+                        <input type="text" />
+                    </form>
+
+                    <form action="" class="form">
+                        <label className="cartContact label" for="">
+                            Email{" "}
+                        </label>
+                        <input type="text" />
+                    </form>
+
+                    <form action="" class="form">
+                        <label className="cartContact label" for="">
+                            Message
+                        </label>
+                        <textarea type="text" />
+                    </form>
+                </div>
+            </div>
+            <div className="btn-knapp">
+                <div className="btn-clear">
+                    <Button
+                        className="cartContact"
+                        text="clear"
+                        style={clearButton}
+                    />
+                </div>
+                <div className="btn-esnd">
+                    <Button
+                        className="cartContact"
+                        text="Send"
+                        style={sendButton}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Contact;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
