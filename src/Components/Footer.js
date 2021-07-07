153e187697f46d3cb4bc6fe32255006b32655148
Patrick Svensson <ps.digitalepost@gmail.com>
2021-07-07 12:27:50 +0200

    Add rel to <a> in Footer.js. Fix bug on row 129 in Contact.css.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,29 +1,23 @@
-import {
-    FaPinterestSquare,
-    FaFacebookSquare,
-    FaTwitterSquare,
-    FaInstagramSquare,
-    FaYoutubeSquare,
-} from "react-icons/fa";
+import { FaPinterestSquare, FaFacebookSquare, FaTwitterSquare, FaInstagramSquare, FaYoutubeSquare } from "react-icons/fa";
 import "./Footer.css";
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="footerLinkContainer">
-                <a href="https://www.pinterest.ca/" target="_blank">
+                <a href="https://www.pinterest.ca/" target="_blank" rel="noopener noreferrer">
                     <FaPinterestSquare className="footerLink" />
                 </a>
-                <a href="https://sv-se.facebook.com/" target="_blank">
+                <a href="https://sv-se.facebook.com/" target="_blank" rel="noopener noreferrer">
                     <FaFacebookSquare className="footerLink" />
                 </a>
-                <a href="https://twitter.com/?lang=sv" target="_blank">
+                <a href="https://twitter.com/?lang=sv" target="_blank" rel="noopener noreferrer">
                     <FaTwitterSquare className="footerLink" />
                 </a>
-                <a href="https://www.instagram.com/" target="_blank">
+                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
                     <FaInstagramSquare className="footerLink" />
                 </a>
-                <a href="https://www.youtube.com/" target="_blank">
+                <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
                     <FaYoutubeSquare className="footerLink" />
                 </a>
             </div>
